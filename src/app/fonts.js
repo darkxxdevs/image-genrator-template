9b41c0ec3ec7e974d7779f36a74d062a5454edb7
arpit <arpit2003y@gmail.com>
2023-08-26 13:05:42 +0530

    added files

---
+++ src/app/fonts.js
@@ -0,0 +1,14 @@
+import { Rubik_Iso, Cinzel } from 'next/font/google'
+
+
+
+export const rbFont = Rubik_Iso({
+    subsets: ['latin'],
+    weight: '400'
+})
+
+export const cin = Cinzel({
+    subsets: ['latin'],
+    weight: '400'
+
+})
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,69 @@
+"use client"
+import './page.css'
+import default_image from '../../public/assets/robot.jpg'
 import Image from 'next/image'
-import styles from './page.module.css'
+import Main from './Components/Main/page'
+import { cin } from './fonts'
+import { useRef, useState } from 'react'
 
 export default function Home() {
+
+  const [imageUrl, setImageUrl] = useState("/")
+
+  let inputRef = useRef(null)
+
+  const genImage = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    const response = await fetch()
+  }
+
+  const hidePlaceholder = () => {
+    if (inputRef.current.placeholder !== '') {
+      inputRef.current.placeholder = '';
+    }
+    else {
+      inputRef.current.placeholder = 'Describe what you want to see';
+
+    }
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="home-container">
+      <Main />
+      <div className="card">
+        <div className="title">
+          <h1 className="title-text">AI IMAGE GENRATOR</h1>
+        </div>
+        <div className="image-section">
+          <Image
+            src={imageUrl == "/" ? default_image : imageUrl}
+            style={
+              {
+                height: "90%",
+                width: "30%",
+                borderRadius: '20px'
+              }
+            }
+
+          />
+          <div className="loader"></div>
+        </div>
+
+        <div className="input">
+          <div className="search-box">
+            <input type="text" ref={inputRef} placeholder='Describe what you want to see ' onClick={hidePlaceholder} className='search-input' />
+            <button className='genrate' onClick={genImage}>GENERATE</button>
+
+          </div>
+
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
